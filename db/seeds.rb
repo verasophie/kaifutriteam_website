e88c43fe04c10b6432dc0726f693202ced56c92a
verasophie <vera@j2marketing.de>
2020-04-04 15:26:47 +0200

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: '„First off the Bike“ hieß es bei Vera mit der drittschnellsten Radzeit des Tages und von dieser Position ließ sie sich auch beim Laufen nicht mehr vertreiben und siegte verdient mit einer Zeit von 2:17:31 Stunden.'
+    )
+end
+
+puts '10 blog posts created'
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts '5 skills created'
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: 'My great service',
+    body: 'Johanna und Melina beendeten den Triathlon ebenfalls in den Top 10 und sicherten somit den Kaifu Damen II hinter den starken Läuferinnen des TTS Tri Team den 2. Platz in der Tagesteamwertung.',
+    main_image: 'http://placehold.it/600x400',
+    thumb_image: 'http://placehold.it/350x200' 
+  )
+end
+
+puts '9 portfolio items created'
