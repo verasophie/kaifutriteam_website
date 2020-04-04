280b2458712e7c6b8cfee809613e68e73259f759
verasophie <vera@j2marketing.de>
2020-04-04 16:02:25 +0200

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,22 @@
 class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
-  end  
+  end 
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'You just uploaded a portfolio item.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end 
 end
 
