4e60e9659902b837201970c89c82eb7f3cd29028
verasophie <vera@j2marketing.de>
2020-04-10 09:21:19 +0200

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")      
     end
   end
+
+  def set_copyright
+    VerasViewTool::Renderer.copyright 'Vera', 'Welcome to my page!'
+  end
+
 end
