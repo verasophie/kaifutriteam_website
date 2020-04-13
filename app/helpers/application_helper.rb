52466375da2efecd7ce121a66d98ccef78629298
verasophie <vera@j2marketing.de>
2020-04-13 17:14:08 +0200

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a? GuestUser
-      (link_to "register", new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to "sign in", new_user_session_path)
+      (link_to "register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "sign in", new_user_session_path, class: style)
     else
-      link_to "logout", destroy_user_session_path, method: :delete
+      link_to "logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
