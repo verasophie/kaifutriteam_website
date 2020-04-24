0a12b60b85e3a4a57d9ff9fea2442b913031deeb
Patrick Schnetger <patrick.schnetger@gmail.com>
2020-04-24 12:29:33 +0200

    Use webpacker for jquery and bootstrap
    remove the import oth the js js dependencies with <script> tags and
    instead add the js dependencies via yarn and import them in the
    application.js pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,30 +1,15 @@
-window.alert("Hej du");
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
+import Rails from "@rails/ujs"
+
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("popper.js")
+require("bootstrap")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-//= require jquery3
-//= require jquery
-//= require jquery-ui
-//= require jquery-ui/widget
-//= require jquery-ui/sortable
-//= require popper
-//= require bootstrap-sprockets
-//= require html.sortable
-//= require cocoon
-//= require gritter
-//= require turbolinks
-//= require_tree .
+Rails.start();
