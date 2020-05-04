31cd07ee865111e45473577bce79f98c55a96ea3
verasophie <vera@j2marketing.de>
2020-05-04 08:19:35 +0200

    implement actioncable first steps according to rails guide

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,28 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def guest_user
+      guest = GuestUser.new
+      guest.id = guest.object_id
+      guest.name = "Guest Dude"
+      guest.first_name = "Guest"
+      guest.last_name = "Dude"
+      guest.email = "guest@dude.com"
+      guest
+    end
+ 
+    def connect
+      self.current_user = find_verified_user || guest_user
+    end
+ 
+    private
+      def find_verified_user
+        if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
